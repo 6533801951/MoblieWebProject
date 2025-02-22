696e372fd45424308f75c1949e4f20e1baf6346e
Teeraphat.k <treeraphat.k@kkumail.com>
2025-02-22 23:52:18 +0700

    Update App.js

--- Expo/myapp/App.js
+++ Expo/myapp/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
+import LoginPhoneScreen from "./screens/LoginPhoneScreen";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="LoginPhoneScreen" component={LoginPhoneScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
