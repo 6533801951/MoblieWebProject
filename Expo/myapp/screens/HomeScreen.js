62f0cb8b54ca551f1bb43f742ea64ae273de499c
Teeraphat.k <treeraphat.k@kkumail.com>
2025-02-22 14:44:18 +0700

    Update HomeScreen.js

---
+++ Expo/myapp/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🏠 Welcome to Home Screen!</Text>
+      <Button title="Logout" onPress={() => navigation.navigate("Login")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
