8e7103d67f9b6bf26459b6eb19c908b50f78a2e1
Teeraphat.k <treeraphat.k@kkumail.com>
2025-02-22 14:38:17 +0700

    Del screens

--- Expo/myapp/App.js
+++ Expo/myapp/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "C:\Users\mamm0\Documents\Storage\Work\MoblieWebProject\Expo\myapp\screens\LoginScreen.js";
+import RegisterScreen from "C:\Users\mamm0\Documents\Storage\Work\MoblieWebProject\Expo\myapp\screens\RegisterScreen.js";
 
 const Stack = createStackNavigator();
 
